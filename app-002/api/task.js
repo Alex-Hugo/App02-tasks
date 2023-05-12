fd2a3aecca530dc5b91077c481faa18a81b0ff2c
Alex-Hugo <alexandre.00000830224@unicap.br>
2023-05-12 00:51:32 -0300

    commit tasks

--- app-002/api/task.js
+++ app-002/api/task.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://parseapi.back4app.com/classes/",
   headers: {
-    "X-Parse-Application-Id": "eFczibVN2ivGQXeCfNMCFXuDJceLxGyAxvJ5PExy",
-    "X-Parse-REST-API-Key": "D1sxqz5tqBUWujf7DMRi72V9h8QGBuNDRTQDBtTY",
+    "X-Parse-Application-Id": "rOvi11Jb1gCgmdkXCx1xTmUBjTB86ejziGfj4Rsl",
+    "X-Parse-REST-API-Key": "3loJiIFwzL9cjSz1EY6we5io4UNGanrA4SeS2ScK",
   },
 });
 
--- app-002/screens/TaskScreen.js
+++ app-002/screens/TaskScreen.js
@@ -1,23 +1,58 @@
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import React, { useState } from "react";
+import { ActivityIndicator, FlatList, Text, View, TextInput, Button } from "react-native";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-
 import { CardTask } from "../components/CardTask";
-import { getTasks, updateTask } from "../api/task";
+import { getTasks, createTask, updateTask, deleteTask } from "../api/task";
 
 export const TaskScreen = ({ navigation }) => {
+
   const queryClient = useQueryClient();
+  const [newTaskText, setNewTaskText] = useState('');
+
   const { isLoading, error, data, isFetching } = useQuery({
     queryKey: ["tasks"],
     queryFn: getTasks,
   });
+  console.log("data",data)
 
   const mutation = useMutation({
     mutationFn: updateTask,
     onSuccess: () => {
-      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: (error) => {
+      console.log('error', error);
+    }
+  });
+
+   const createTaskMutation = useMutation({
+    mutationFn: createTask,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      setNewTaskText('');
+    },
+  });
+
+   const deleteTaskMutation = useMutation({
+    mutationFn: deleteTask,
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["tasks"] });
     },
   });
+
+  const handleNewTaskTextChange = (text) => {
+    setNewTaskText(text);
+  };
+
+  const handleCreateTask = () => {
+    if (newTaskText.trim() !== '') {
+      createTaskMutation.mutate({ description: newTaskText });
+    }
+  };  
+
+  const handleDeleteTask = (objectId) => {
+    deleteTaskMutation.mutate(objectId);
+  };
 
   if (isLoading) {
     return (
@@ -45,7 +80,13 @@ export const TaskScreen = ({ navigation }) => {
       }}
     >
       {isFetching && <Text>IS FETCHING</Text>}
-      <FlatList
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1, width:100 }}
+        onChangeText={handleNewTaskTextChange}
+        value={newTaskText}
+      />
+     <Button title="Create Task" onPress={handleCreateTask} />
+     <FlatList
         style={{ flex: 1 }}
         data={data.results}
         keyExtractor={(item) => item.objectId}
@@ -54,6 +95,7 @@ export const TaskScreen = ({ navigation }) => {
             task={item}
             navigation={navigation}
             taskDoneChange={mutation.mutate}
+            onDeletePress={(objectId) => handleDeleteTask(objectId)}
           />
         )}
       />
